2856121e8f2ba0f85cdf3779846583015428d5e9
Ganjalf99 <colombaniemanuele@gmail.com>
2021-05-12 12:22:56 +0200

    prova design new workout

--- app/src/main/java/com/example/workout_tracker/fragment/exercise/NewTrainingFragment.kt
+++ app/src/main/java/com/example/workout_tracker/fragment/exercise/NewTrainingFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.workout_tracker.R
 import com.example.workout_tracker.util.Exercise
+import com.example.workout_tracker.util.Workout
 import kotlinx.android.synthetic.main.fragment_newtraining.*
 import kotlinx.android.synthetic.main.fragment_startworkout.*
 
@@ -22,7 +23,16 @@ class NewTrainingFragment: Fragment(R.layout.fragment_newtraining){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        var workout = Workout("Petto/bicipiti")
+        var es1 = Exercise("pancaPiana,",3,10,120)
+        var es2 = Exercise("curlbilli,",3,12,120)
+        var es3 = Exercise("pancaPiana,",3,4,120)
+        workout.addExercise(es1)
+        Log.d("WorkoutFragment",workout.toString())
+        workout.addExercise(es2)
+        Log.d("WorkoutFragment",workout.toString())
+        workout.removeExercise(es1)
+        Log.d("WorkoutFragment",workout.toString())
     }
 
 
