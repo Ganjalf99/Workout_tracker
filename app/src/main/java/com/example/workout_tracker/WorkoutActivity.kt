469326069c27a9a08d8869656301dae73c4fabbd
Ganjalf99 <colombaniemanuele@gmail.com>
2021-06-05 22:30:15 +0200

    -Dynamically fill workout activity with the choosen workout

--- app/src/main/java/com/example/workout_tracker/WorkoutActivity.kt
+++ app/src/main/java/com/example/workout_tracker/WorkoutActivity.kt
@@ -1,5 +1,7 @@
 package com.example.workout_tracker
 import android.content.Intent
+import android.graphics.Paint
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
@@ -15,6 +17,7 @@ import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
+import androidx.core.view.setPadding
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
@@ -35,25 +38,41 @@ class WorkoutActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_workout)
+        var i=0;
         val workout = intent.getSerializableExtra("Workout") as Workout
         textViewNomeWorkout.text = workout.nome
+        textViewNomeWorkout.paintFlags = textViewNomeWorkout.paintFlags or Paint.UNDERLINE_TEXT_FLAG
             workout.exerciseList.forEach {
+                i++
                 var textView: TextView = TextView(this)
-                textView.text="${it.nome}"
+                textView.text="$i) ${it.nome}"
+                textView.textSize = 24f
+                textView.setPadding(24)
+                textView.typeface = Typeface.DEFAULT_BOLD
                 linear_layout_vertical.addView(textView)
 
 
                 for ( i in 0 until it.serie){
                     var linearLayout : LinearLayout = LinearLayout(this)
                     linear_layout_vertical.addView(linearLayout)
+
                     var textView2: TextView = TextView(this)
+                    textView2.text="${i+1}° Serie"
+                    textView2.setPadding(70)
+                    textView2.textSize = 18f
                     var edittxtserie: EditText = EditText(this)
                     edittxtserie.hint = "N° rep"
                     edittxtserie.inputType = InputType.TYPE_CLASS_NUMBER
+                    edittxtserie.setPadding(70)
+                    edittxtserie.textSize = 18f
                     var edittxtpeso: EditText = EditText(this)
                     edittxtpeso.hint = "Kg"
                     edittxtpeso.inputType = InputType.TYPE_CLASS_NUMBER
-                    textView2.text="${i+1}° Serie"
+                    edittxtpeso.setPadding(70)
+                    edittxtpeso.textSize = 18f
+
+
+
                     linearLayout.addView(textView2)
                     linearLayout.addView(edittxtserie)
                     linearLayout.addView(edittxtpeso)
